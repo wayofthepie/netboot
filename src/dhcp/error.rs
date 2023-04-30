9b2ad9e675207e938225a52e541fa8e7d687935d
wayofthepie <wayofthepie@gmail.com>
2023-05-01 00:04:53 +0100

    Refactor into modules

---
+++ src/dhcp/error.rs
@@ -0,0 +1,24 @@
+use nom::error::{ErrorKind, ParseError};
+
+#[derive(Debug)]
+pub enum DHCPMessageError<I> {
+    InvalidDataError,
+    NotYetImplemented,
+    NomError(nom::error::Error<I>),
+}
+
+impl<'a> From<nom::error::Error<&'a [u8]>> for DHCPMessageError<&'a [u8]> {
+    fn from(e: nom::error::Error<&'a [u8]>) -> Self {
+        DHCPMessageError::NomError(e)
+    }
+}
+
+impl<I> ParseError<I> for DHCPMessageError<I> {
+    fn from_error_kind(input: I, kind: ErrorKind) -> Self {
+        DHCPMessageError::NomError(nom::error::Error::new(input, kind))
+    }
+
+    fn append(_input: I, _kind: ErrorKind, other: Self) -> Self {
+        other
+    }
+}
