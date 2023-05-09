a6c62bfe049ed142a9de110f1bf9e24ac77d2ff7
wayofthepie <wayofthepie@gmail.com>
2023-05-09 20:51:40 +0100

    Add work on handler

---
+++ src/handler.rs
@@ -0,0 +1,205 @@
+use std::{
+    net::{Ipv4Addr, SocketAddr},
+    str::FromStr,
+};
+
+use futures::{Sink, SinkExt, TryStream, TryStreamExt};
+
+use crate::dhcp::{
+    self, DhcpMessage, DhcpOption, DhcpOptionValue, DhcpOptions, MessageType, Operation,
+};
+
+pub struct Handler<St, Si> {
+    stream: St,
+    sink: Si,
+}
+
+impl<St, Si> Handler<St, Si>
+where
+    St: TryStreamExt + Unpin,
+    St: TryStream<Ok = (DhcpMessage, SocketAddr), Error = Box<dyn std::error::Error + Send + Sync>>,
+    Si: Sink<(DhcpMessage, SocketAddr), Error = Box<dyn std::error::Error + Send + Sync>> + Unpin,
+    Si: SinkExt<(DhcpMessage, SocketAddr)>,
+{
+    pub fn new(stream: St, sink: Si) -> Self {
+        Self { stream, sink }
+    }
+
+    pub async fn handle(&mut self) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        while let Some((msg, _)) = self.stream.try_next().await? {
+            tracing::debug!("{:#?}", msg);
+            match msg.operation {
+                dhcp::Operation::Discover => self.handle_discover(msg).await,
+                dhcp::Operation::Offer => todo!(),
+                dhcp::Operation::Request => todo!(),
+                dhcp::Operation::Acknowledgement => todo!(),
+            }
+        }
+        Ok(())
+    }
+
+    async fn handle_discover(&mut self, mut dhcp: DhcpMessage) {
+        dhcp.operation = Operation::Offer;
+        dhcp.your_address = Ipv4Addr::from_str("192.168.122.204").unwrap();
+        dhcp.server_address = Ipv4Addr::from_str("192.168.122.1").unwrap();
+        let mut options = DhcpOptions::new();
+        options.insert(
+            DhcpOption::MessageType,
+            DhcpOptionValue::MessageType(MessageType::Offer),
+        );
+        options.insert(
+            DhcpOption::SubnetMask,
+            DhcpOptionValue::SubnetMask(Ipv4Addr::from_str("255.255.255.0").unwrap()),
+        );
+        options.insert(
+            DhcpOption::Router,
+            DhcpOptionValue::Router(Ipv4Addr::from_str("192.168.122.1").unwrap()),
+        );
+        dhcp.options = options;
+        self.sink
+            .send((dhcp, SocketAddr::from_str("255.255.255.255:68").unwrap()))
+            .await
+            .unwrap();
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::{
+        collections::HashMap,
+        error::Error,
+        net::{Ipv4Addr, SocketAddr},
+        pin::Pin,
+        str::FromStr,
+        task::{Context, Poll},
+    };
+
+    use futures::Sink;
+    use tokio::sync::mpsc;
+    use tokio_stream::wrappers::ReceiverStream;
+
+    use crate::dhcp::{
+        DhcpMessage, DhcpOption, DhcpOptionValue, Flags, HardwareType, MessageType, Operation,
+    };
+
+    use super::Handler;
+    pub struct FakeSink<'a> {
+        buffer: &'a mut Vec<(DhcpMessage, SocketAddr)>,
+    }
+
+    impl<'a> FakeSink<'a> {
+        pub fn new(buffer: &'a mut Vec<(DhcpMessage, SocketAddr)>) -> Self {
+            Self { buffer }
+        }
+    }
+
+    impl<'a> Sink<(DhcpMessage, SocketAddr)> for FakeSink<'a> {
+        type Error = Box<dyn Error + Send + Sync>;
+
+        fn poll_ready(
+            self: Pin<&mut Self>,
+            _cx: &mut Context<'_>,
+        ) -> Poll<Result<(), Self::Error>> {
+            Poll::Ready(Ok(()))
+        }
+
+        fn start_send(
+            mut self: Pin<&mut Self>,
+            item: (DhcpMessage, SocketAddr),
+        ) -> Result<(), Self::Error> {
+            self.buffer.push(item);
+            Ok(())
+        }
+
+        fn poll_flush(
+            self: Pin<&mut Self>,
+            _cx: &mut Context<'_>,
+        ) -> Poll<Result<(), Self::Error>> {
+            Poll::Ready(Ok(()))
+        }
+
+        fn poll_close(
+            self: Pin<&mut Self>,
+            _cx: &mut Context<'_>,
+        ) -> Poll<Result<(), Self::Error>> {
+            Poll::Ready(Ok(()))
+        }
+    }
+
+    fn build_dhcp_message() -> DhcpMessage {
+        DhcpMessage {
+            operation: Operation::Discover,
+            hardware_type: HardwareType::Ethernet,
+            hardware_len: 6,
+            hops: 0,
+            xid: 0,
+            seconds: 0,
+            flags: Flags { broadcast: true },
+            client_address: Ipv4Addr::from_str("0.0.0.0").unwrap(),
+            your_address: Ipv4Addr::from_str("0.0.0.0").unwrap(),
+            server_address: Ipv4Addr::from_str("0.0.0.0").unwrap(),
+            gateway_address: Ipv4Addr::from_str("0.0.0.0").unwrap(),
+            client_hardware_address: vec![0, 0, 0, 0, 0, 0],
+            options: HashMap::new(),
+        }
+    }
+
+    fn expected_offer() -> DhcpMessage {
+        let options_vec = vec![
+            (
+                DhcpOption::MessageType,
+                DhcpOptionValue::MessageType(MessageType::Offer),
+            ),
+            (
+                DhcpOption::SubnetMask,
+                DhcpOptionValue::SubnetMask(Ipv4Addr::from_str("255.255.255.0").unwrap()),
+            ),
+            (
+                DhcpOption::Router,
+                DhcpOptionValue::Router(Ipv4Addr::from_str("192.168.122.1").unwrap()),
+            ),
+        ];
+        let options = HashMap::from_iter(options_vec);
+        DhcpMessage {
+            operation: Operation::Offer,
+            hardware_type: HardwareType::Ethernet,
+            hardware_len: 6,
+            hops: 0,
+            xid: 0,
+            seconds: 0,
+            flags: Flags { broadcast: true },
+            client_address: Ipv4Addr::from_str("0.0.0.0").unwrap(),
+            your_address: Ipv4Addr::from_str("192.168.122.204").unwrap(),
+            server_address: Ipv4Addr::from_str("192.168.122.1").unwrap(),
+            gateway_address: Ipv4Addr::from_str("0.0.0.0").unwrap(),
+            client_hardware_address: vec![0, 0, 0, 0, 0, 0],
+            options,
+        }
+    }
+
+    #[tokio::test]
+    async fn should_handle_discover() {
+        let (tx, rx) = mpsc::channel::<
+            Result<(DhcpMessage, SocketAddr), Box<dyn std::error::Error + Send + Sync>>,
+        >(16);
+        tx.send(Ok((
+            build_dhcp_message(),
+            SocketAddr::from_str("255.255.255.255:0").unwrap(),
+        )))
+        .await
+        .unwrap();
+        tx.send(Err("done".into())).await.unwrap();
+        let fake_stream = ReceiverStream::new(rx);
+        let mut buffer = vec![];
+        let fake_sink = FakeSink::new(&mut buffer);
+        let mut handler = Handler::new(fake_stream, fake_sink);
+        let _ = handler.handle().await;
+        let result = buffer.get(0).unwrap();
+        let expected_result = expected_offer();
+        assert_eq!(
+            result.0, expected_result,
+            "expected\n{:#?}\ngot\n{:#?}",
+            expected_result, result.0
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
 mod dhcp;
+mod handler;
 
-use std::net::Ipv4Addr;
-use std::net::SocketAddr;
-use std::str::FromStr;
-
-use dhcp::{
-    DhcpCodec, DhcpMessage, DhcpOption, DhcpOptionValue, DhcpOptions, MessageType, Operation,
-};
-use futures::stream::SplitSink;
-use futures::SinkExt;
+use dhcp::DhcpCodec;
 use futures::StreamExt;
-use futures::TryStreamExt;
+use handler::Handler;
 use tokio::net::UdpSocket;
 use tokio_util::udp::UdpFramed;
 use tracing_subscriber::prelude::*;
@@ -33,42 +26,7 @@ async fn init() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let sock = UdpSocket::bind("0.0.0.0:67").await?;
     sock.set_broadcast(true)?;
 
-    let (mut sink, mut stream) = UdpFramed::new(sock, DhcpCodec::new()).split();
-    while let Some((msg, _)) = stream.try_next().await? {
-        tracing::debug!("{:#?}", msg);
-        match msg.operation {
-            dhcp::Operation::Discover => handle_discover(msg, &mut sink).await,
-            dhcp::Operation::Offer => todo!(),
-            dhcp::Operation::Request => todo!(),
-            dhcp::Operation::Acknowledgement => todo!(),
-        }
-    }
-    Ok(())
-}
-
-async fn handle_discover(
-    mut dhcp: DhcpMessage,
-    sink: &mut SplitSink<UdpFramed<DhcpCodec>, (DhcpMessage, SocketAddr)>,
-) {
-    dhcp.operation = Operation::Offer;
-    dhcp.your_address = Ipv4Addr::from_str("192.168.122.204").unwrap();
-    dhcp.server_address = Ipv4Addr::from_str("192.168.122.1").unwrap();
-    let mut options = DhcpOptions::new();
-    options.insert(
-        DhcpOption::MessageType,
-        DhcpOptionValue::MessageType(MessageType::Offer),
-    );
-    options.insert(
-        DhcpOption::SubnetMask,
-        DhcpOptionValue::SubnetMask(Ipv4Addr::from_str("255.255.255.0").unwrap()),
-    );
-    options.insert(
-        DhcpOption::Router,
-        DhcpOptionValue::Router(Ipv4Addr::from_str("192.168.122.1").unwrap()),
-    );
-
-    dhcp.options = options;
-    sink.send((dhcp, SocketAddr::from_str("255.255.255.255:68").unwrap()))
-        .await
-        .unwrap();
+    let (sink, stream) = UdpFramed::new(sock, DhcpCodec::new()).split();
+    let mut handler = Handler::new(stream, sink);
+    handler.handle().await
 }
